7d2925f9b6097423e3f152db86fc35dd260e9fcc
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 02:07:19 +0530

    Python Variables Intro

---
+++ PythonVariables.py
@@ -0,0 +1,33 @@
+''' Python Variables  - Assign Multiple Values '''
+
+# 1. Many Values to multiple variables
+w, x, y, z= "Gaurav", "Vaibhav", "Kartik", "Aryaman" 
+print(x, y, z, w)
+
+# 2. One Value to multiple variables
+x = y = z = 5
+print(x, y, z)
+
+# 3. List 
+fruits = ["apple", "Grapes", "Cherry", "banana", "straberry", "watermelon"]
+print(fruits) # index 
+
+''' Output Variables '''
+x = "vaibhav is a pygame developer"
+print(x)
+
+a = "kartik" 
+b = "is a web developer"
+# kartik is a web developer
+
+''' Concatenation '''
+# This is for string
+
+a = "kartik " 
+b = "is a web developer"
+print(a + b)
+
+# This is totally for numbers
+a = 5
+b = 7
+print(a + b)
