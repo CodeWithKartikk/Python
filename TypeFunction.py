c0f237dc82316376efcb279a5cbe6e990de5f020
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 02:02:20 +0530

    Type Function in Python

---
+++ TypeFunction.py
@@ -0,0 +1,26 @@
+# Type function
+
+num = 5.26545   'ab'
+t = type(num)
+print(num)
+print(t)
+
+# Example-1
+x=1
+y=561516516420589519
+z=597
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Example-2
+p=12155
+q=215144444.59659
+r=-56.6595
+print(type(p))
+print(type(q))
+print(type(r))
+
+# Example-3
+cx = 3+4j
+print(type(complex))
