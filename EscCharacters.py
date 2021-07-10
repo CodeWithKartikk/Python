9e838865b2fa665c5e2fa5214591adf4223e4725
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 02:19:16 +0530

    Escape Characters in Python

---
+++ EscCharacters.py
@@ -0,0 +1,15 @@
+# Escape Characters - To insert characters that are illegal in a string then we have to use an escape characters
+
+#    Code           Result
+#     \'          Single Quote
+#     \\          Backslash
+#     \n          New Line
+#     \r          Carriage Return
+#     \t          Tab
+#     \b          backspace
+#     \f          form feed
+#    \ooo         Octal Value
+#    \xhh         Hex Value
+
+txt = "My name is \"Vaibhav\" and I am in \"8th std.\""
+print(txt) 
