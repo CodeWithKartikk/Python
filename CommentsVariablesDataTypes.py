b37c7c4975569513cbe8205281da415e966c7b7c
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 02:01:00 +0530

    Comments, Variables and Data Types in Python

---
+++ CommentsVariablesDataTypes.py
@@ -0,0 +1,45 @@
+'''
+Author : Kartik Kathuria
+Topic : Learning Python
+Time : 12:23
+'''
+
+# Types of comments 
+# 1. Single line comment
+# 2. Multi line comment
+
+
+# Variables
+vaibhavAge = 15 #(camelCase Rule)
+a = 5
+b = 7
+c = 10
+
+# Type Casting
+a = 5
+a = float(5)
+print(a)
+print(type(a))
+
+# DataTypes -- (Data can be stored at memory location according to the type of the data)
+
+# 1. Primary DataTypes
+"""
+1. byte -- 1byte 
+2. int -- 4 bytes
+3. short -- 2 bytes
+4. long -- 8 bytes 
+5. float -- 4 bytes
+6. double -- 8 bytes
+7. char -- 1 byte
+8. Boolean -- 1 bit (True{1} or False{0})
+"""
+
+# 2. Refrence DataTypes
+'''
+1. string
+2. List
+3. Tuple
+4. Dict
+5. Set 
+'''
